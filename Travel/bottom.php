89d937873e8e0f037c151a43489481e87be227f3
rakshith kiran <rakshithkiran79@gmail.com>
2020-05-28 12:33:45 +0530

    Initial Commit

---
+++ Travel/bottom.php
@@ -0,0 +1,64 @@
+<div class="fotter">	
+	 <div class="container">
+		 <div class="fotter-grids">
+			 <div class="col-md-4 fotter-left">
+			 <img src="images/fotter-logo.png" alt="">
+			 <p style="text-align:justify">We here at Amigo Tours provide exclusive and affordable tours. We have been working as a Tours & Travel Agency for the last five years with Americans, Australians and Spanish groups around the most important places in the World.</p>
+			 </div>
+			 <div class="col-md-4 fotter-middle">
+			 <br>
+				 <h3>Questions About Our Tours?</h3>
+				 <div class="footer-list">
+						<ul>
+                        <li style="text-align:justify"><span></span>
+Our Customer Service is always happy to answer any questions that you might have. Please visit our Contact Page to contact us or call us at 9119229334.</li></ul> </div>
+						<h3>why choose us?</h3>
+                        <div class="footer-list">
+                        <ul>
+
+ <li style="text-align:justify"><span></span>Customer Satisfaction<br/></li>
+ <li style="text-align:justify"><span></span>Superior customer service with 24x7 Dedicated helpline and over 15 million delighted customers & still growing.<br/></li>
+<li style="text-align:justify"><span></span>Best Deals Guaranteed<br/></li>
+<li style="text-align:justify"><span></span>Great experiences at lowest prices guaranteed.<br/></li>
+</a></li> </ul>
+			 </div>
+             
+			 </div>
+			 <div class="col-md-4 fotter-right" style="padding-left:70px">
+			 <h3>Why buy from us?</h3><br/>
+              <div class="footer-list">
+			  <ul>
+              <li style="color:#FFF;text-align:justify"><a style="color:#FFF"><span></span> Payment Security</a></li>
+              <li style="color:#FFF;text-align:justify"><a style="color:#FFF"><span></span> Travel Guide</a></li>
+              <li style="color:#FFF;text-align:justify"><a style="color:#FFF"><span></span> Social Responsibility</a></li>
+               </ul> </div></div>
+			 <div class="social-icons">
+			 <a href="http://www.facebook.com" target="_blank"><span class="facebook"> </span></a>
+			 <a href="http://www.twitter.com" target="_blank"><span class="twitter"> </span></a>
+			 <a href="http://www.pinterest.com" target="_blank"><span class="pinterest"> </span></a>
+			 <a href="http://www.instagram.com" target="blank"><span class="instagram"> </span></a>
+			 </div>
+			 <div class="clearfix"></div>
+	     </div>
+		 <div class="clearfix"></div>
+	 </div>
+</div>
+</div>  
+<div class="copyright text-right">
+<p style="padding-right:100px">Designed by: Team Amigo</p>
+</div>
+  <script src="js/jquery.scrollTo.js"></script>
+	<script src="js/jquery.nav.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function() {
+		$('#nav').onePageNav({
+			begin: function() {
+			console.log('start')
+			},
+			end: function() {
+			console.log('stop')
+			}
+		});
+	});
+	</script>
+  
